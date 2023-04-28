bc137330226f18fbca012f6927a8cbab734d12bb
Jieun huh <112235808+JIeunhuh@users.noreply.github.com>
2023-04-28 18:56:32 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 204 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//30명 중에 과제 제출 안한 두명 찾아서 출력
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean stu[] = new boolean[31];
+		for (int i = 0; i < 28; i++) {
+			stu[sc.nextInt()] = true;
+
+		}
+		for (int i =1 ; i <= 30; i++) {
+			if (!stu[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
